522fa5a6e5eb19b24edf8fb5e01faaff49c034b6
neothethird <neo@neothethird.de>
2018-03-04 14:07:26 +0100

    Removed remnant

--- qml/libs/pastebin.js
+++ qml/libs/pastebin.js
@@ -1,6 +1,3 @@
-
-var PASTEBIN_API_KEY = "";//need an API Dev key for this
-var PASTEBIN_API_URL = "http://pastebin.com/api/api_post.php";
 var UBUNTU_PASTEBIN_URL="http://paste.ubuntu.com/"
 
 function post(message, name, on_success, on_failure)
@@ -30,35 +27,6 @@ function post(message, name, on_success, on_failure)
                 on_success(UBUNTU_PASTEBIN_URL+ result+ "/");
 
 
-            } else
-                on_failure(response);
-        }
-    }
-
-    req.send(args.join('&'));
-}
-
-function postoPasteBin(message, title, on_success, on_failure)
-{
-    var args = new Array();
-
-    args.push( "api_dev_key=" + PASTEBIN_API_KEY);
-    args.push( "api_option=paste");
-    args.push( "api_paste_code=" + encodeURIComponent( message));
-    args.push( "api_paste_name=" + encodeURIComponent( title));
-    args.push( "api_paste_private=1") // not litsed
-    args.push( "api_paste_expire_date=1M"); // expires in a month
-
-    var req = new XMLHttpRequest();
-    req.open("post", PASTEBIN_API_URL);
-    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    req.onreadystatechange = function() {
-        if(req.readyState === XMLHttpRequest.DONE) {
-            var response = req.responseText;
-            if(response.toLowerCase().indexOf("bad") != 0) { // "Bad xxx: yyy"
-
-                on_success(response);
             } else
                 on_failure(response);
         }
